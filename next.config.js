ae527a390a19c2cce62684a191166baebddebf8e
ganam <7ganam@gmail.com>
2023-01-20 22:18:06 +0200

    ignore eslint during build

--- next.config.js
+++ next.config.js
@@ -1,28 +1,31 @@
 // Enable this code below for Server Side Rendering/Translation (SSR)
 // const { i18n } = require('./next-i18next.config')
-const withImages = require('next-images');
-const ESLintPlugin = require('eslint-webpack-plugin');
+const withImages = require("next-images");
 
 module.exports = withImages({
   // Enable this code below for Server Side Rendering/Translation (SSR)
   //  i18n,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   useTrailingSlash: true,
   images: {
-    disableStaticImages: true
+    disableStaticImages: true,
   },
   publicRuntimeConfig: {
-    localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
-      ? process.env.LOCALE_SUBPATHS
-      : 'none',
+    localeSubpaths: typeof process.env.LOCALE_SUBPATHS === "string" ? process.env.LOCALE_SUBPATHS : "none",
   },
-  webpack: (config, options) => {
+  webpack: (config) => {
     cssModules: true,
-    config.plugins.push(
-      //      new ESLintPlugin({
-      //        exclude: ['node_modules']
-      //      })
-    );
-    config.node = {}
+      config.plugins
+        .push
+        //      new ESLintPlugin({
+        //        exclude: ['node_modules']
+        //      })
+        ();
+    config.node = {};
     return config;
   },
 });
