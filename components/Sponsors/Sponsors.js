d8aaa8acdb4ffc5616c6d9d249a7e244409aeb7f
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-02-14 13:53:51 +0200

    ref: replace the adu logo with the nyu adu log

--- components/Sponsors/Sponsors.js
+++ components/Sponsors/Sponsors.js
@@ -1,22 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { useTheme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import ScrollAnimation from "react-scroll-animation-wrapper";
-import clsx from "clsx";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
-import { useTranslation } from "next-i18next";
-import imgAPI from "~/public/images/imgAPI";
 import SponsorCard from "../Cards/Sponsor";
 import useTitle from "../Title/title-style";
 import useStyles from "./sponsor-style";
 
-import s1 from "~/public/images/sponsors/ADU logo.png";
-import s2 from "~/public/images/sponsors/Altair logo.png";
+import s1 from "~/public/images/sponsors/NYUAbuDhabiLogo.png";
 import s3 from "~/public/images/sponsors/ASME logo.png";
 import s4 from "~/public/images/sponsors/EVIS logo.jpg";
 import s5 from "~/public/images/sponsors/Global EEE logo.png";
@@ -26,56 +19,10 @@ import s8 from "~/public/images/sponsors/3elemLogo.png";
 import s9 from "~/public/images/sponsors/Al Nabooda logo.png";
 import s10 from "~/public/images/sponsors/projotecLogo.png";
 import s11 from "~/public/images/sponsors/usm-uae-seal.png";
-const categories = ["all", "Orthopedic", "Nutritionist", "Pediatric", "Anaesthestic"];
-
-const doctorsData = [
-  {
-    avatar: imgAPI.medical[7],
-    name: "Jena Doe",
-    title: "Pediatric Surgeon",
-    rating: 95,
-    exp: 4,
-  },
-  {
-    avatar: imgAPI.medical[12],
-    name: "John Doe",
-    title: "Pediatric Surgeon",
-    rating: 95,
-    exp: 4,
-  },
-  {
-    avatar: imgAPI.medical[8],
-    name: "Jihan Doe",
-    title: "Pediatric Surgeon",
-    rating: 95,
-    exp: 4,
-  },
-  {
-    avatar: imgAPI.medical[10],
-    name: "James Doe",
-    title: "Pediatric Surgeon",
-    rating: 95,
-    exp: 4,
-  },
-  {
-    avatar: imgAPI.medical[11],
-    name: "Jim Doe",
-    title: "Pediatric Surgeon",
-    rating: 95,
-    exp: 4,
-  },
-  {
-    avatar: imgAPI.medical[9],
-    name: "Jessy Doe",
-    title: "Pediatric Surgeon",
-    rating: 95,
-    exp: 4,
-  },
-];
 
 const sponsorsData = [
   { logo: s5, title: "Global EEE", website: "https://globaleee.org/", role: "Technical Organizer" },
-  { logo: s1, title: "ADU", website: "https://www.adu.ac.ae", role: "Academic Partner" },
+  { logo: s1, title: "NYU ADU", website: "https://nyuad.nyu.edu/en/", role: "Academic Partner" },
   { logo: s8, title: "3elm", website: "https://3elmeducation.com/ ", role: "Education Partner" },
   { logo: s11, title: "US Embassy", website: "https://ae.usembassy.gov/", role: "AbuDhabi,UAE" },
   { logo: s10, title: "ProjoTech", website: "https://projotech.com/", role: "EdTech Partner" },
@@ -91,16 +38,8 @@ function Sponsors() {
   const theme = useTheme();
   const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
 
-  // Translation Function
-  const { t } = useTranslation("common");
-
   const classes = useStyles();
   const title = useTitle();
-  const [selectedIndex, setSelectedIndex] = useState("all");
-
-  function handleListItemClick(event, index) {
-    setSelectedIndex(index);
-  }
 
   const renderCard = (item, index) => (
     <SponsorCard key={index.toString()} website={item.website} title={item.title} logo={item.logo} role={item.role} />
@@ -114,8 +53,8 @@ function Sponsors() {
           <Grid item md={2} sm={9} xs={12}>
             <ScrollAnimation animateOnce animateIn="fadeInLeftShort" offset={-100} delay={200} duration={0.3}>
               <div className={classes.sideFilter}>
-                <Typography variant="h4" className={title.primary}>
-                  Partners
+                <Typography variant="h4" className={`${title.primary} whitespace-nowrap`}>
+                  Partners and Sponsors
                 </Typography>
               </div>
             </ScrollAnimation>
